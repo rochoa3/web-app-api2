8548c6478b45981a169bdff9e6ec62d6218684dd
rochoa3 <54958768+rochoa3@users.noreply.github.com>
2022-02-17 02:05:11 -0500

    Add files via upload

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        tirm: true,
+        lowercase: true
+    },
+    password: {
+        type:String,
+        required: true,
+        trim: true,
+        minLength: 8
+    },
+    name: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
+
+    
